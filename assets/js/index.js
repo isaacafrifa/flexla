67a3240918af99c9ab2fea6ec87b86c236db6fa1
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-02-01 11:02:11 +0000

    added header and implemented nav toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navigation = document.querySelector('.navigation')
+const navCloseBtn = document.querySelector('.close-btn')
+const blurOverlay = document.querySelector('.blur-overlay')
+
+const IS_ACTIVE = 'is-active'
+const CLICK = 'click'
+
+//toggle navigation and blur overlay
+const toggleNavigation = () => {
+    navigation.classList.toggle(IS_ACTIVE)
+    blurOverlay.classList.toggle(IS_ACTIVE)
+}
+
+//listeners
+menuBtn.addEventListener(CLICK, toggleNavigation)
+navCloseBtn.addEventListener(CLICK, toggleNavigation)
+blurOverlay.addEventListener(CLICK, toggleNavigation) 
+
 //create new instance of fullpage constructor
-new fullpage('#myfullpage',{
+new fullpage('#myfullpage', {
     autoScrolling: true,
     scrollBar: true
 })
